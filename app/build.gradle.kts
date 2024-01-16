e6fce3d66e4afb3c6009332c48ad2ca48f55e25c
Akhil Balakrishnan <akhilbkrishnan2000@gmail.com>
2024-01-16 16:10:21 +0530

    Migrated from kotlin annotation processing tool (kapt) to kotlin symbol processing (ksp)
    Also updated lifecycle-runtime.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 
-    id("kotlin-kapt")
+    // KSP plugin
+    id("com.google.devtools.ksp")
+    // Dagger Hilt plugin
     id("com.google.dagger.hilt.android")
+    // Kotlin parcelize plugin
     id("kotlin-parcelize")
 }
 
@@ -75,7 +78,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -98,7 +101,7 @@ dependencies {
 
     //Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.50")
-    kapt("com.google.dagger:hilt-compiler:2.50")
+    ksp("com.google.dagger:hilt-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     //Retrofit
@@ -123,6 +126,6 @@ dependencies {
 
     //Room
     implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 
+    // Dagger Hilt
     id("com.google.dagger.hilt.android") version "2.50" apply false
+    // Kotlin serialization
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
+    // KSP annotation processor plugin
+    id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false
 }
